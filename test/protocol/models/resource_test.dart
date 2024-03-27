f5f6341a89c18dc84ddf38b668c1709732dd66d4
Ethan Lee <ethanl@tbd.email>
2024-03-26 17:01:46 -0700

    add resource test

---
+++ test/protocol/models/resource_test.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:tbdex/src/protocol/models/resource.dart';
+import 'package:test/test.dart';
+import 'package:web5/web5.dart';
+
+import '../../test_data.dart';
+
+void main() async {
+  await TestData.initializeDids();
+
+  group('ResourceTest', () {
+    test('sign populates resource signature', () async {
+      final offering = TestData.getOffering();
+      await offering.sign(TestData.pfiDid);
+
+      expect(offering.signature, isNotNull);
+      final jws = Jws.decode(offering.signature!);
+      expect(jws.header.alg, isNotNull);
+      expect(jws.header.kid, contains(TestData.pfiDid.uri));
+    });
+
+    test('parse throws error if json string is not valid', () {
+      expect(() => Resource.parse(';;;;'), throwsA(isA<FormatException>()));
+    });
+
+    test('resources must be signed by the sender', () async {
+      final offeringFromPfi = TestData.getOffering();
+      // Sign it with the wrong DID
+      await offeringFromPfi.sign(TestData.aliceDid);
+
+      expect(
+        () => Resource.parse(jsonEncode(offeringFromPfi.toJson())),
+        throwsA(
+          isA<Exception>().having(
+            (e) => e,
+            'message',
+            contains(
+              'Signature verification failed: Was not signed by the expected DID',
+            ),
+          ),
+        ),
+      );
+    });
+  });
+}
